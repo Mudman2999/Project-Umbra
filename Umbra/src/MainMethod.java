f63e9d1052592c98b7e112e56e1eb7c61d17bc32
Grant <grantgoodwin00@gmail.com>
2015-02-20 12:26:02 -0500

    Platform Generation

--- Umbra/src/MainMethod.java
+++ Umbra/src/MainMethod.java
@@ -1,6 +1,7 @@
 
 public class MainMethod {
 	public static void main(String[] args) {
-		Window  mainWin = new Window();
+		MapGenerator h = new MapGenerator();
+		//Window  mainWin = new Window();
 	}
 }
--- Umbra/src/MapGenerator.java
+++ Umbra/src/MapGenerator.java
@@ -1,4 +1,16 @@
-
 public class MapGenerator {
-
+	int upper = 30;
+	int lower = 5;
+	int left = 0;
+	int right = 800;
+	int mapHeights [] = new int[3];
+	int mapDistances [] = new int[3];
+	MapGenerator(){
+		for(int i = 0; i < 3; i++){
+		mapHeights[i] =  (int) (Math.random() * (upper - lower)) + lower;
+		mapDistances[i] =  (int) (Math.random() * (right - left)) + left;
+		System.out.println(mapHeights[i]);
+		System.out.println(mapDistances[i]);
+	}
+}
 }
